10aa07fa77deba899b3f01f8de728ed0d04eee57
Vaishnaviiii-23 <vaish3229@gmail.com>
2025-08-15 15:13:43 +0530

    added rqumnt

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS (allow your frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://deepdoc.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
